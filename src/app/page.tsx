ffbc04e9136500103f69e1eed46dd5abd71f6625
GIGA-CODER <112710558+Aditya-Sakpal@users.noreply.github.com>
2024-10-18 19:43:13 +0000

    initial UI setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,13 @@ const page = () => {
     <>
     <div className='w-[100vw] h-[100vh] flex justify-center items-center ' >
       <div className='w-[50%] h-[100%] border border-black flex flex-col justify-start items-center ' >
-        <h1>Mortgages</h1>
+        <h1 className='text-2xl font-bold' >Mortgages</h1>
+        <div className='w-[100%] h-[95%] border border-black ' >
+        </div>
       </div>
       <div className='w-[50%] h-[100%] border border-black flex flex-col justify-start items-center ' >
-        <h1>Mortgage-process</h1>
+        <h1 className='text-2xl font-bold'>Mortgage-process</h1>
+        <div className='w-[100%] h-[95%] border border-black '></div>
       </div>
     </div>
     </>
