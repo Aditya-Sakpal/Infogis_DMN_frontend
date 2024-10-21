1d653eb5a9a6a0845b7e76ee148c06e321773bfd
Aditya-Sakpal <aditya.as@somaiya.edu>
2024-10-21 15:04:57 +0530

    minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,39 @@ import React from 'react'
 const page = () => {
   return (
     <>
-    <div className='w-[100vw] h-[100vh] flex justify-center items-center ' >
-      <div className='w-[50%] h-[100%] border border-black flex flex-col justify-start items-center ' >
-        <h1 className='text-2xl font-bold' >Mortgages</h1>
-        <div className='w-[100%] h-[95%] border border-black ' >
+      <div className='w-[100vw] h-[100vh] flex flex-wrap justify-center items-center  ' >
+        <div className='w-[25%] h-[40%] shadow-2xl rounded-xl m-[2%] ' >
+          <div className='w-[100%] h-[20%]  flex justify-center items-center ' >
+            <h1 className='text-2xl font-semibold ' >Age Check</h1>
+          </div>
+          <div className='w-[100%] h-[80%] px-[5%]  flex flex-col justify-start items-center ' >
+            <div className='w-[100%] h-[15%] my-[5%]  flex justify-start items-center ' >
+              <label htmlFor="" className='w-[20%] flex items-center justify-center font-semibold ' >Age : </label>
+              <input type="number" placeholder='Enter the age of the applicant...' className='border border-black w-[80%] rounded-3xl text-center h-[100%]' />
+            </div>
+            <input type="submit" className='w-[20%] h-[15%]  rounded-3xl bg-[#2596be] ' />
+          </div>
         </div>
+        <div className='w-[25%] h-[40%] shadow-2xl rounded-xl m-[2%] ' >
+          <div className='w-[100%] h-[20%]  flex justify-center items-center ' >
+            <h1 className='text-2xl font-semibold ' >Bankrutcy Check</h1>
+          </div>
+          <div className='w-[100%] h-[80%] px-[5%]  flex flex-col justify-start items-center ' >
+            <div className='w-[100%] h-[15%] my-[5%]  flex justify-start items-center ' >
+              <label htmlFor="" className='w-[20%] flex items-center justify-center font-semibold ' >Year of Occurence : </label>
+              <input type="number" placeholder='Enter the year of occurence...' className='border border-black w-[80%] rounded-3xl text-center h-[100%]' />
+            </div>
+            <div className='w-[100%] h-[15%] my-[5%]  flex justify-start items-center ' >
+              <label htmlFor="" className='w-[20%] flex items-center justify-center font-semibold ' >Amount Owed : </label>
+              <input type="number" placeholder='Enter the amount owed...' className='border border-black w-[80%] rounded-3xl text-center h-[100%]' />
+            </div>
+            <input type="submit" className='w-[20%] h-[15%]  rounded-3xl bg-[#2596be] ' />
+          </div>
+        </div>
+        <div className='w-[25%] h-[40%] shadow-2xl rounded-xl m-[2%] ' ></div>
+        <div className='w-[25%] h-[40%] shadow-2xl rounded-xl m-[2%] ' ></div>
+        <div className='w-[25%] h-[40%] shadow-2xl rounded-xl m-[2%] ' ></div>
       </div>
-      <div className='w-[50%] h-[100%] border border-black flex flex-col justify-start items-center ' >
-        <h1 className='text-2xl font-bold'>Mortgage-process</h1>
-        <div className='w-[100%] h-[95%] border border-black '></div>
-      </div>
-    </div>
     </>
   )
 }
